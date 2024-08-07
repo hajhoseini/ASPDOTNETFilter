c55f54bbf7bf0d51d29145eaecd9a2c2a240ee90
Azam HajMohammadHoseini <hoseini.1988@gmail.com>
2024-08-07 13:41:55 -0700

    example for AuthorizationFilter

--- AuthorizationFilter/Controllers/HomeController.cs
+++ AuthorizationFilter/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using AuthorizationFilter.Models;
+using AuthorizationFilter.Models.Filters;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -27,6 +28,18 @@ namespace AuthorizationFilter.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [CustomAuthorizationFilter("admin")]
+        public IActionResult Action1()
+        {
+            return View();
+        }
+
+        [CustomAuthorizationFilter("customer")]
+        public IActionResult Action2()
+        {
+            return View();
         }
     }
 }
